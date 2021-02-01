c88dc5946cedba0a264f6d7dcbdff3eb2e2ccdce
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-02-01 15:14:12 +0300

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -13,15 +13,16 @@ public function __construct($value1,$result, $value2,$operator){
     $this->result = $result; 
 }
 public function Calc(){
-    //Take the values from the form
+       //Take the values from the form
 	if(isset($_POST['submit'])) {
 	$this->value1 = $_POST['value1'];
 	$this->value2 = $_POST['value2'];
-    $this->operator = $_POST['operator'];
-    
+        $this->operator = $_POST['operator'];
+           //check if the inputs are numeric
 	    if (is_numeric($this->value1) && is_numeric($this->value2)) {
-
+            //check what operator has been chosen
             switch ($this->operator) {
+	       //Addition
                 case '+':
                     $this->result = $this->value1 + $this->value2;
                         return $this->result;
@@ -29,6 +30,7 @@ public function Calc(){
                         return $this->value2;
                         return $this->value1;
                     break;
+	        //substraction
                 case '-':
                     $this->result = $this->value1 - $this->value2;
                         return $this->result;
@@ -36,6 +38,7 @@ public function Calc(){
                         return $this->value2;
                         return $this->value1;
                     break;
+	        //Division
                 case '/':
                 $this->result = $this->value1 / $this->value2;
                         return $this->result;
@@ -43,6 +46,7 @@ public function Calc(){
                         return $this->value2;
                         return $this->value1;
                     break;
+	        //multiplication
                 case '*':
                     $this->result= $this->value1 * $this->value2;
                         return $this->result;
@@ -50,11 +54,12 @@ public function Calc(){
                         return $this->value2;
                         return $this->value1;
                     break;               
-                
+                //Else return error
                 default:
                     echo "NON EXISTING OPERATOR!!!";
                     break;
-            }	
+            }
+         //if the number is not numberix
         }else{
                 echo "ERROR CANNOT CALCULATE!!. <br> Please enter numeric values only";
             }
