3a3c02842dd186474d460054f6a457f00ec0c68e
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-10-14 11:48:25 +0300

    Added js & css cdn

--- index.php
+++ index.php
@@ -4,13 +4,9 @@ include 'calculator.php';
 <!DOCTYPE html>
 <html> 
 <head>
-	<title>Calculator In PHP</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<title>Calculator In PHP</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </head> 
 <body class="body">
 	<div class="jumbotron text-center">
